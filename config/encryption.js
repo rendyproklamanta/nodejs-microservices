0ef38670b26fb605807a52a0bd15663d7d9c4a56
rendyproklamanta <rendyproklamanta@gmail.com>
2023-09-15 17:02:04 +0700

    update

--- config/encryption.js
+++ config/encryption.js
@@ -1,48 +1,19 @@
-const crypto = require('crypto');
+const Cryptr = require('cryptr');
+const cryptr = new Cryptr(
+   process.env.ENCRYPTION_KEY,
+   {
+      encoding: 'base64',
+      pbkdf2Iterations: 10000,
+      saltLength: 10
+   });
 
-const secretKey = crypto.randomBytes(32);
-const secretIV = crypto.randomBytes(16);
-const algorithm = 'aes-256-cbc';
-
-if (!secretKey || !secretIV || !algorithm) {
-   throw new Error('secretKey, secretIV, and algorithm are required');
+function encrypt(val) {
+   return cryptr.encrypt(val);
 }
 
-// Generate secret hash with crypto to use for encryption
-const key = crypto
-   .createHash('sha512')
-   .update(secretKey)
-   .digest('hex')
-   .substring(0, 32);
-
-const encryptionIV = crypto
-   .createHash('sha512')
-   .update(secretIV)
-   .digest('hex')
-   .substring(0, 16);
-
-// Encrypt data
-const encrypt = (val) => {
-   const cipher = crypto.createCipheriv(algorithm, key, encryptionIV);
-
-   // Encrypts data and converts to hex and base64
-   return Buffer.from(
-      cipher.update(val, 'utf8', 'hex') + cipher.final('hex')
-   ).toString('base64');
-};
-
-// Decrypt data
-const decrypt = (val) => {
-   const buff = Buffer.from(val, 'base64');
-   const decipher = crypto.createDecipheriv(algorithm, key, encryptionIV);
-
-   // Decrypts data and converts to utf8
-   return (
-      decipher.update(buff.toString('utf8'), 'hex', 'utf8') +
-      decipher.final('utf8')
-   );
-};
-
+function decrypt(val) {
+   return cryptr.decrypt(val);
+}
 module.exports = {
    encrypt,
    decrypt,
